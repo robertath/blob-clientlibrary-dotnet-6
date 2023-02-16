007bb28d67680af2c1ce725b1631f9fe8513b06f
robertath <robertath@gmail.com>
2023-02-16 20:27:50 +0000

    initial

---
+++ BlobClientLibrary/Program.cs
@@ -0,0 +1,196 @@
+﻿using Azure;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System.ComponentModel;
+
+Console.WriteLine("Azure Blob Storage exercise\n");
+
+// Run the examples asynchronously, wait for the results before proceeding
+ProcessAsync().GetAwaiter().GetResult();
+
+Console.WriteLine("Press enter to exit the sample application.");
+Console.ReadLine();
+
+
+static async Task ProcessAsync()
+{
+    string localPath = "./data/";
+    string fileName = "wtfile" + Guid.NewGuid().ToString() + ".txt";
+    string localFilePath = Path.Combine(localPath, fileName);
+    string downloadFilePath = localFilePath.Replace(".txt", "DOWNLOADED.txt");
+
+
+    // Copy the connection string from the portal in the variable below.
+    string storageConnectionString = "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
+
+    // Create a client that can authenticate with a connection string
+    BlobServiceClient blobServiceClient = new BlobServiceClient(storageConnectionString);
+
+    Console.WriteLine("Step create Blob container");
+    BlobContainerClient containerClient = await CreateContainer(blobServiceClient);
+
+    // Get a reference to the blob
+    BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+    Console.WriteLine("The next step is upload blob, create file");
+    await UploadToContainerAsync(localFilePath, blobClient, containerClient);
+
+    Console.WriteLine("The next step is download blob file to local path.");
+    await DownloadBlobToLocalFile(blobClient, localFilePath);
+    
+    Console.WriteLine("The next step is add container metadata properties.");
+    await AddContainerMetadataAsync(containerClient);
+
+    Console.WriteLine("The next step is read container metadata properties.");
+    await ReadContainerMetadataAsync(containerClient);
+
+    Console.WriteLine("The next step is retrieve container properties.");
+    await ReadContainerPropertiesAsync(containerClient);
+    
+    Console.WriteLine("The next step is to delete the container and local files.");    
+    await DeleteContainerFromBlob(containerClient, localFilePath, downloadFilePath);
+}
+
+
+static async Task<BlobContainerClient> CreateContainer(BlobServiceClient blobServiceClient)
+{
+    //Create a unique name for the container
+    string containerName = "wtblob" + Guid.NewGuid().ToString();
+
+    // Create the container and return a container client object
+    BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
+    Console.WriteLine("A container named '" + containerName + "' has been created. " +
+        "\nTake a minute and verify in the portal." + 
+        "\nNext a file will be created and uploaded to the container.");
+    Console.WriteLine("Press 'Enter' to continue.");
+    Console.ReadLine();
+
+    return containerClient;
+}
+
+static async Task UploadToContainerAsync(string localFilePath, BlobClient blobClient, BlobContainerClient containerClient)
+{
+    // Write text to the file
+    await File.WriteAllTextAsync(localFilePath, "Hello, World!");
+
+    Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", blobClient.Uri);
+
+    // Open the file and upload its data
+    using (FileStream uploadFileStream = File.OpenRead(localFilePath))
+    {
+        await blobClient.UploadAsync(uploadFileStream);
+        uploadFileStream.Close();
+    }
+
+    Console.WriteLine("\nThe file was uploaded. We'll verify by listing" + 
+            " the blobs next.");
+    
+    
+    Console.WriteLine("Listing blobs...");
+    await foreach (BlobItem blobItem in containerClient.GetBlobsAsync())
+    {
+        Console.WriteLine("\t" + blobItem.Name);
+    }
+
+    Console.WriteLine("\nYou can also verify by looking inside the " + 
+            "container in the portal." +
+            "\nNext the blob will be downloaded with an altered file name.");
+    Console.WriteLine("Press 'Enter' to continue.");
+    Console.ReadLine();
+}
+
+static async Task DownloadBlobToLocalFile(BlobClient blobClient, string downloadFilePath)
+{
+    Console.WriteLine("\nDownloading blob to\n\t{0}\n", downloadFilePath);
+
+    // Download the blob's contents and save it to a file
+    BlobDownloadInfo download = await blobClient.DownloadAsync();
+
+    using (FileStream downloadFileStream = File.OpenWrite(downloadFilePath))
+    {
+        await download.Content.CopyToAsync(downloadFileStream);
+    }
+    Console.WriteLine("\nLocate the local file in the data directory created earlier to verify it was downloaded.");
+    Console.WriteLine("Press 'Enter' to continue.");
+
+}
+
+static async Task DeleteContainerFromBlob(BlobContainerClient containerClient, string localFilePath, string downloadFilePath)
+{
+    // Delete the container and clean up local files created
+    Console.WriteLine("\n\nDeleting blob container...");
+    await containerClient.DeleteAsync();
+
+    Console.WriteLine("Deleting the local source and downloaded files...");
+    File.Delete(localFilePath);
+    File.Delete(downloadFilePath);
+
+    Console.WriteLine("Finished cleaning up.");
+}
+
+static async Task ReadContainerPropertiesAsync(BlobContainerClient container)
+{
+    try
+    {
+        // Fetch some container properties and write out their values.
+        var properties = await container.GetPropertiesAsync();
+        Console.WriteLine($"Properties for container {container.Uri}");
+        Console.WriteLine($"Public access level: {properties.Value.PublicAccess}");
+        Console.WriteLine($"Last modified time in UTC: {properties.Value.LastModified}");
+        Console.WriteLine("Press 'Enter' to continue.");
+        Console.ReadLine();
+    }
+    catch (RequestFailedException e)
+    {
+        Console.WriteLine($"HTTP error code {e.Status}: {e.ErrorCode}");
+        Console.WriteLine(e.Message);
+        Console.ReadLine();
+    }
+}
+
+static async Task AddContainerMetadataAsync(BlobContainerClient container)
+{
+    try
+    {
+        IDictionary<string, string> metadata =
+           new Dictionary<string, string>();
+
+        // Add some metadata to the container.
+        metadata.Add("docType", "textDocuments");
+        metadata.Add("category", "guidance");
+
+        // Set the container's metadata.
+        Console.WriteLine("Adding container's metadata...");
+        await container.SetMetadataAsync(metadata);        
+        Console.WriteLine("Press 'Enter' to continue.");
+        Console.ReadLine();
+    }
+    catch (RequestFailedException e)
+    {
+        Console.WriteLine($"HTTP error code {e.Status}: {e.ErrorCode}");
+        Console.WriteLine(e.Message);
+        Console.ReadLine();
+    }
+}
+
+static async Task ReadContainerMetadataAsync(BlobContainerClient container)
+{
+    try
+    {
+        var properties = await container.GetPropertiesAsync();
+
+        // Enumerate the container's metadata.
+        Console.WriteLine("Container metadata:");
+        foreach (var metadataItem in properties.Value.Metadata)
+        {
+            Console.WriteLine($"\tKey: {metadataItem.Key}");
+            Console.WriteLine($"\tValue: {metadataItem.Value}");
+        }
+    }
+    catch (RequestFailedException e)
+    {
+        Console.WriteLine($"HTTP error code {e.Status}: {e.ErrorCode}");
+        Console.WriteLine(e.Message);
+        Console.ReadLine();
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# Client Library Console for Azure Blob
+
+### Tech
+- dotnet 6.0
+- Storage account Emulator > Blob Container
+- c#
+
+### Lab
+[link](https://learn.microsoft.com/en-us/training/modules/work-azure-blob-storage/3-develop-blob-storage-dotnet)
+
+### Example with features:
+- Add blob containers from library
+- Add a new file
+- Transfer to local file 
+- Add metadata information
+- Read metadata
+- Read properties of container
+
+## Storage account emulator
+Set up azurite
+```
+C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\Extensions\Microsoft\Azure Storage Emulator\azurite.exe
+```
